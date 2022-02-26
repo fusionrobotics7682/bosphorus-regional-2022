ae8ad3e2277064a1a373b6e39db94aa12c8045dc
Burak Talha <alptng72@gmail.com>
2022-02-26 14:53:42 +0300

    Remove default comment (attempt)

--- teleop/Command_DISF/src/main/java/frc/robot/RobotContainer.java
+++ teleop/Command_DISF/src/main/java/frc/robot/RobotContainer.java
@@ -49,12 +49,7 @@ public class RobotContainer {
 
   }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a 
-   * {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
+
   private void configureButtonBindings() {
 
     // Running sequential
@@ -68,11 +63,7 @@ public class RobotContainer {
   }
 
 
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
+
   public Command getAutonomousCommand() {
     
     // 1 Ball point and returning terminal
