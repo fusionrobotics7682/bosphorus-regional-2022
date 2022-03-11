cb8abbdbc01eb870c96f50f2ad12c69e8f14d119
okcu73 <alptng72@gmail.com>
2022-03-12 01:01:54 +0300

    Add output speed constraints

--- src/main/java/frc/robot/commands/Teleop/Unit/Drive/ArcadeDriveCommand.java
+++ src/main/java/frc/robot/commands/Teleop/Unit/Drive/ArcadeDriveCommand.java
@@ -2,49 +2,38 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.Teleop.Unit.Drive;
+package frc.robot.commands.Teleop.unit.Drive;
 
-import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.DriveSubsystem;
 
 public class ArcadeDriveCommand extends CommandBase {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  final DriveSubsystem driveSubsystem;
   
-  private DriveSubsystem driveSubsystem;
-  Timer timer = new Timer();
-  double xSpeed;
-  double zSpeed;
-  double xAxis;
-  double zAxis;
-
-  public ArcadeDriveCommand(DriveSubsystem driveSubsystem,double xSpeed, double zSpeed, double xAxis, double zAxis) {
+  /** Creates a new ArcadeDriveCommand. */
+  public ArcadeDriveCommand(DriveSubsystem driveSubsystem) {
     this.driveSubsystem = driveSubsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(driveSubsystem);
   }
-    
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    System.out.println("DriveCommand initialized");
+    System.out.println("ArcadeDriveCommand.initialize()");
+    driveSubsystem.differentialDrive.setMaxOutput(0.8);
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    if(xSpeed < 0.8 && zSpeed < 0.8){
-    driveSubsystem.arcadeDrive(xSpeed*xAxis, zSpeed*zAxis);
-    }
-    else{
-      driveSubsystem.arcadeDrive(0,0);
-    }
+    driveSubsystem.arcadeDrive();
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    driveSubsystem.stopDrive();
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
--- src/main/java/frc/robot/commands/Teleop/Unit/Drive/TankDriveCommand.java
+++ src/main/java/frc/robot/commands/Teleop/Unit/Drive/TankDriveCommand.java
@@ -2,23 +2,40 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands.Teleop.Unit.Drive;
+package frc.robot.commands.Teleop.unit.Drive;
 
+import frc.robot.subsystems.DriveSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
+/** An example command that uses an example subsystem. */
 public class TankDriveCommand extends CommandBase {
-  /** Creates a new AxisTankDriveCommand. */
-  public TankDriveCommand() {
+  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
+  final DriveSubsystem driveSubsystem;
+
+
+  /**
+   * Creates a new ExampleCommand.
+   *
+   * @param subsystem The subsystem used by this command.
+   */
+  public TankDriveCommand(DriveSubsystem driveSubsystem) {
+    this.driveSubsystem = driveSubsystem;
     // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(driveSubsystem);
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {}
+  public void initialize() {
+    System.out.println("TankDriveCommand.initialize()");
+    driveSubsystem.differentialDrive.setMaxOutput(0.7);
+  }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
-  public void execute() {}
+  public void execute() {
+    driveSubsystem.tankDrive();
+  }
 
   // Called once the command ends or is interrupted.
   @Override
