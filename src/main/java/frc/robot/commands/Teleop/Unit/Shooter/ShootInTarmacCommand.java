92228222f59b0ebb826ba50398ee8e91e2eeaa37
okcu73 <alptng72@gmail.com>
2022-03-12 01:11:31 +0300

    Add Shooter tele-op commands

---
+++ src/main/java/frc/robot/commands/Teleop/Unit/Shooter/ShootInTarmacCommand.java
@@ -0,0 +1,42 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.Teleop.unit.Shooter;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.ShooterSubsystem;
+
+public class ShootInTarmacCommand extends CommandBase {
+
+  ShooterSubsystem shooterSubsystem;
+
+  /** Creates a new ShootToHub. */
+  public ShootInTarmacCommand(ShooterSubsystem shooterSubsystem) {
+    this.shooterSubsystem = shooterSubsystem;
+    // Use addRequirements() here to declare subsystem dependencies.
+    addRequirements(shooterSubsystem);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {}
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    shooterSubsystem.shoot(0.5, 0.7);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    shooterSubsystem.stopMotor();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
