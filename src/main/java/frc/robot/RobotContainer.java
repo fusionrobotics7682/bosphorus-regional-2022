87711c52878bee1a3406d3701df8a68609926c92
Burak Talha <alptng72@gmail.com>
2022-03-15 20:55:21 +0300

    conf

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,25 +4,15 @@
 
 package frc.robot;
 
-import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.Joystick;
-import edu.wpi.first.wpilibj.XboxController;
-import frc.robot.commands.Autonomous.polymorphic.ThreeBallScenarios.MidThreeBallCommand;
-import frc.robot.commands.Autonomous.polymorphic.TwoBallScenarios.LowerTwoBallCommand;
-import frc.robot.commands.Autonomous.polymorphic.TwoBallScenarios.MidTwoBallCommand;
-import frc.robot.commands.Autonomous.polymorphic.TwoBallScenarios.UpperTwoBallCommand;
-import frc.robot.commands.Path.TwoBallPathCommand;
+import frc.robot.commands.Teleop.binary.*;
+import frc.robot.commands.Teleop.unit.Drive.ArcadeDriveCommand;
+import frc.robot.commands.Teleop.unit.Feeder.GetInFeederCommand;
+import frc.robot.commands.Teleop.unit.Feeder.GetOutFeederCommand;
 import frc.robot.commands.Teleop.unit.Drive.*;
-import frc.robot.commands.Teleop.unit.Feeder.*;
-import frc.robot.commands.Teleop.unit.Intake.*;
-import frc.robot.commands.Teleop.unit.Lift.*;
-import frc.robot.commands.Teleop.unit.Shooter.*;
-import frc.robot.commands.Teleop.binary.GetBallWithFeederCommand;
-import frc.robot.commands.Teleop.binary.ShootWithFeederCommand;
 import frc.robot.subsystems.DriveSubsystem;
 import frc.robot.subsystems.FeederSubsystem;
 import frc.robot.subsystems.IntakeSubsystem;
-import frc.robot.subsystems.LiftSubsystem;
 import frc.robot.subsystems.ShooterSubsystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
@@ -35,77 +25,53 @@ import edu.wpi.first.wpilibj2.command.button.JoystickButton;
  */
 public class RobotContainer {
   // The robot's subsystems and commands are defined here...
-
-  // Subsystems
   private final DriveSubsystem driveSubsystem = new DriveSubsystem();
   private final IntakeSubsystem intakeSubsystem = new IntakeSubsystem();
-  private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
   private final FeederSubsystem feederSubsystem = new FeederSubsystem();
-  private final LiftSubsystem liftSubsystem = new LiftSubsystem();
+  private final ShooterSubsystem shooterSubsystem = new ShooterSubsystem();
 
-  // Commands
-
-  // Path Commands
-
-      // Two Ball Path Commands
-      TwoBallPathCommand twoBallPathCommand = new TwoBallPathCommand(driveSubsystem);
-
-  // Gyro|Encoder Commands
-
-      // Two Ball Scenarios
-      LowerTwoBallCommand lowerTwoBallCommand = new LowerTwoBallCommand(driveSubsystem, intakeSubsystem, feederSubsystem, shooterSubsystem);
-      MidTwoBallCommand midTwoBallCommand = new MidTwoBallCommand(driveSubsystem, intakeSubsystem, feederSubsystem, shooterSubsystem);
-      UpperTwoBallCommand upperTwoBallCommand = new UpperTwoBallCommand(driveSubsystem, intakeSubsystem, feederSubsystem, shooterSubsystem);
-
-      // Three Ball Scenarios
-      MidThreeBallCommand midThreeBallCommand = new MidThreeBallCommand(driveSubsystem, intakeSubsystem, feederSubsystem, shooterSubsystem);
-
-  // Controller
   Joystick joystick = new Joystick(0);
-
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-
-    driveSubsystem.setDefaultCommand(new TankDriveCommand(driveSubsystem));
-
     // Configure the button bindings
     configureButtonBindings();
-  }
-
-  /**
-   * Use this method to define your button->command mappings. Buttons can be created by
-   * instantiating a {@link GenericHID} or one of its subclasses ({@link
-   * edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then passing it to a {@link
-   * edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {
-    // Unit process
-    new JoystickButton(joystick, 1).whileActiveContinuous(new GetInFeederCommand(feederSubsystem));
-    new JoystickButton(joystick, 2).whileActiveContinuous(new GetOutFeederCommand(feederSubsystem));
-    new JoystickButton(joystick, 3).whileActiveContinuous(new GetInTakeCommand(intakeSubsystem));
-    new JoystickButton(joystick, 4).whileActiveContinuous(new GetOutTakeCommand(intakeSubsystem));
-    new JoystickButton(joystick, 5).whileActiveContinuous(new ShootInTarmacCommand(shooterSubsystem));
-    new JoystickButton(joystick, 6).whileActiveContinuous(new LiftUpCommand(liftSubsystem));
-
-    // Parallel process
-    new JoystickButton(joystick, 7).whileActiveContinuous(new GetBallWithFeederCommand(feederSubsystem, intakeSubsystem));
-    new JoystickButton(joystick, 8).whileActiveContinuous(new ShootWithFeederCommand(feederSubsystem, shooterSubsystem));
-    // for back
-    // Use getOutFeederCommand and getOutTakeCommand
-  }
-
-  /**
-   * Use this to pass the autonomous command to the main {@link Robot} class.
-   *
-   * @return the command to run in autonomous
-   */
-  public Command getAutonomousCommand() {
-    // An ExampleCommand will run in autonomous
-
-
     
+    driveSubsystem.setDefaultCommand(new ArcadeDriveCommand(driveSubsystem));
 
-    return lowerTwoBallCommand.getCommand();
+  }
+
+
+  private void configureButtonBindings() {
+
+    // Running Binary Commands
+
+    /*
+    new JoystickButton(joystick, 2).whileActiveContinuous(new GetBallWithFeederCommand(feederSubsystem, intakeSubsystem));
+    new JoystickButton(joystick, 3).whileActiveContinuous(new ThrowBallWithFeederCommand(feederSubsystem, shooterSubsystem));
+    */
+
+    // Running unit Commands
+    // Intake
+    new JoystickButton(joystick, 1).whileActiveContinuous(new GetBallIntakeCommand(intakeSubsystem));
+    new JoystickButton(joystick, 4).whileActiveContinuous(new GetOutIntakeCommand(intakeSubsystem));
+    // Feeder
+    new JoystickButton(joystick, 2).whileActiveContinuous(new GetInFeederCommand(feederSubsystem));
+    new JoystickButton(joystick, 3).whileActiveContinuous(new GetOutFeederCommand(feederSubsystem));
+    
+    // Shooter
+    new JoystickButton(joystick, 5).whileActiveContinuous(new ThrowBallShooterCommand(shooterSubsystem));
+    new JoystickButton(joystick, 6).whileActiveContinuous(new GetInBallShooterCommand(shooterSubsystem));
+    
+    // Drive
+    /* I did not write this because it is unneccessary but I left it here for everything */
+    
+  }
+
+
+
+  public Command getAutonomousCommand() {
+
+    return null;
   }
 }
